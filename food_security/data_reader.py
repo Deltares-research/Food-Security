915c0942f9faaf4cd19c50ce69499c94c05bf53e
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-12-05 15:33:33 +0100

    added reader for rice yield data

--- food_security/data_reader.py
+++ food_security/data_reader.py
@@ -1,10 +1,14 @@
 """Data reader module for reading grid and vector data."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import logging
+from typing import TYPE_CHECKING, ClassVar
 
+import pandas as pd
 import rasterio
 from rasterstats import zonal_stats
+
+logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -32,6 +36,34 @@ class Grid:
         for col, stat in cols.items():
             regions = self.get_region_stat(regions, col_name=col, stat=stat)
         return regions
+
+
+
+def read_and_transform_rice_yield_table(file_path: str | Path, year: int) -> None:
+    region_mapper = {
+    "VinghLn_AdvIrr42": "Vihn Long",
+    "CaMau_AdvIrr43": "Ca Mau",
+    "AnGi_AdvIrr44": "An Giang",
+    "DongTh_AdvIrr45": "Dong Thap",
+    "BacLieu_AdvIrr74": "Bac Lieu",
+    "SocTrang_AdvIrr75": "Soc Trang",
+    "KienGi_AdvIrr76": "Kien Giang",
+    "CanTho_AdvIrr77": "Can Tho",
+    "HuaGi_AdvIrr": "Hau Giang",
+    "TraVinh_AdvIrr164": "Tra Vinh",
+    "TienGi_AdvIrr175": "Tien Giang",
+    "BenTre_AdvIrr182": "Ben Tre",
+                    }
+    rice_yield_df = pd.read_excel(file_path, sheet_name="Sheet1")
+    regions = rice_yield_df.iloc[1].dropna().to_numpy()
+    regions = [region_mapper[region] for region in regions]
+    date = f"01-01-{year}"
+    if date not in rice_yield_df.GMT.to_numpy():
+        logger.warning("No rice data found for year %s", year)
+        return None
+    rice_yield = rice_yield_df[rice_yield_df["GMT"] == date].to_numpy()[0][1:]
+    data = [{"region": region, "rice_yield": rice} for region, rice in zip(regions, rice_yield)]
+    return pd.DataFrame(data=data)
 
 
 
--- tests/test_data_reader.py
+++ tests/test_data_reader.py
@@ -1,11 +1,14 @@
-from food_security.data_reader import Grid
+import logging
+
 import geopandas as gpd
+
+from food_security.data_reader import Grid, read_and_transform_rice_yield_table
 
 
 def test_Grid_get_region_stat(regions, grid_file):
     grid = Grid(fp=grid_file)
     region_stats = grid.get_region_stat(
-        regions=regions, col_name="lifestock", stat="sum"
+        regions=regions, col_name="lifestock", stat="sum",
     )
     assert isinstance(region_stats, gpd.GeoDataFrame)
     assert "lifestock" in region_stats.columns
@@ -16,6 +19,17 @@ def test_Grid_get_region_stats(regions, grid_file):
     assert isinstance(region_stats, gpd.GeoDataFrame)
     assert "lifestock_sum" in region_stats.columns
     assert "lifestock_mean" in region_stats.columns
+
+def test_read_and_transform_rice_yield_data(rice_yield_data, caplog):
+    df = read_and_transform_rice_yield_table(rice_yield_data, year=2014)
+    assert "Can Tho" in df.region.to_numpy()
+    assert "Hau Giang" in df.region.to_numpy()
+    caplog.set_level(logging.WARNING)
+    df = read_and_transform_rice_yield_table(rice_yield_data, year=2020)
+    assert "No rice data found for year 2020" in caplog.text
+    assert df is None
+
+
 
 
 
