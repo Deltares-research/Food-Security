c6104489ddccbb16202fa70c023476e43df26e6c
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-12-20 16:55:20 +0100

    implemented export ratio calculations with FAO api data

--- food_security/food_supply.py
+++ food_security/food_supply.py
@@ -6,6 +6,7 @@ import geopandas as gpd
 import pandas as pd
 
 from food_security.base import FSBase
+from food_security.fao_api import get_food_export_df, get_food_production_df
 
 logger = logging.getLogger(__name__)
 
@@ -14,24 +15,26 @@ class FoodSupply(FSBase):
     """Calculate the total food supply based on import and export and food production."""
 
     def __init__(self, cfg: dict, region: gpd.GeoDataFrame) -> None:
-        self.region = region
-        self.eximport_df = pd.read_csv(self.cfg["food_supply"]["export"]["path"])
-        self.total_production_FAO = pd.read_csv(self.cfg["food_supply"]["total_production_FAO"]["path"])
         super().__init__(cfg=cfg)
+        self.region = region
+        self.export_df = get_food_export_df(country_name=self.cfg["main"]["country"], year=self.cfg["main"]["year"])
+        self.total_production_FAO = get_food_production_df(
+            country_name=self.cfg["main"]["country"], year=self.cfg["main"]["year"]
+        )
 
     def get_import(self):
         pass
 
     def get_export(self):
-        export_df = self.eximport_df.copy(deep=True)
+        export_df = self.export_df.copy(deep=True)
         export_df = export_df[
             (export_df["Year"] == self.cfg["main"]["year"]) & (export_df["Element"] == "Export Quantity")
         ]
         food_types = list(self.cfg["food_production"]["lifestock"]["paths"].keys()) + list(
             self.cfg["food_production"]["other_crops"]["paths"].keys(),
         )
-        for food_type in food_types:
-            pass
+
+        # TODO: calculate export of food based on export ratios for country
 
     def get_road_density(self, geometry: gpd.GeoDataFrame):
         pass
@@ -56,8 +59,19 @@ class FoodSupply(FSBase):
     ) -> gpd.GeoDataFrame:
         food_supply_region = self.calculate_food_supply_for_region(geometry=region)
 
-    def _calculate_export_ratio(self, food_items):
-        pass
+    def _calculate_export_ratio(self, food_items: list[str]) -> pd.DataFrame:
+        mapping_df = pd.read_csv(self.cfg["food_supply"]["FAO_item_mapping"]["path"])
+        tp = self.total_production_FAO
+        tt = self.export_df
+
+        export_ratios = []
+        for food in food_items:
+            item_code = str(mapping_df[mapping_df["Item"] == food]["Item_code"].to_numpy()[0])
+            total_production = pd.to_numeric(tp[tp["Item Code"] == item_code]["Value"])
+            total_export = pd.to_numeric(tt[tt["Item Code"] == item_code]["Value"]).sum()
+            export_ratio = total_export / total_production
+            export_ratios.append({"item": food, "export_ratio": export_ratio})
+        return pd.DataFrame(export_ratios)
 
     @property
     def total_production_FAO(self) -> pd.DataFrame:  # noqa: N802
@@ -65,4 +79,4 @@ class FoodSupply(FSBase):
 
     @total_production_FAO.setter
     def total_production_FAO(self, df: pd.DataFrame):  # noqa: N802
-        return df[df["Area"] == self.cfg["main"]["country"]]
+        self._total_production_FAO = df[df["Area"] == self.cfg["main"]["country"]]
--- tests/test_food_supply.py
+++ tests/test_food_supply.py
@@ -13,3 +13,10 @@ def test_FoodSupply_get_export(data_dir):
     cfg = {"food_supply": {"import_export_table": {"path": data_dir / "FAOSTAT_data_en_11-18-2024"}}}
     fs = FoodSupply(cfg=cfg)
     fs.get_export()
+
+
+@pytest.mark.local
+def test_FoodSupply_calculate_export_ratio(regions, example_config):
+    fs = FoodSupply(cfg=example_config, region=regions)
+    food_items = ["chicken", "buffalo", "sugar_cane"]
+    export_ratios = fs._calculate_export_ratio(food_items=food_items)
