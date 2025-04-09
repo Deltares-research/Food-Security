f7f5843eed770f48a554dfe16c948b158b2bba1f
Tjalling de Jong <tjalling.dejong@deltares.nl>
2025-04-09 16:59:41 +0200

    overhauling initial code

--- food_security/food_production.py
+++ food_security/food_production.py
@@ -5,11 +5,8 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING
 
-import pandas as pd
-
-from food_security import DATA_DIR
 from food_security.base import FSBase
-from food_security.data_reader import Grid, HisFile
+from food_security.data_reader import HisFile
 
 if TYPE_CHECKING:
     import geopandas as gpd
@@ -18,68 +15,33 @@ logger = logging.getLogger(__name__)
 
 
 class FoodProduction(FSBase):
-    """FoodProduction class that groups methods for calculating the food production in an area."""
+    """FoodProduction class that groups methods for calculating the food production."""
 
     def __init__(self, cfg: dict, region: gpd.GeoDataFrame) -> None:
         """Instatiate a FoodProduction object.
 
         Args:
             cfg (dict): config dict.
-            region (gpd.GeoDataFrame): GeoDataFrame containing regions with a Name column and geometries.
+            region (gpd.GeoDataFrame): GeoDataFrame containing regions with a Name
+            column and geometries.
 
         """
         self.region = region
         super().__init__(cfg=cfg)
 
-    def add_lifestock(self) -> None:
-        """Calculate the lifestock expressed as kg meat per ha per animal."""
-        paths = self.cfg["food_production"]["lifestock"].get("paths")
-        animal_yield = pd.read_csv(DATA_DIR / "animal_yield.csv")
-        for key, path in paths.items():
-            logging.info("Reading %s grid data", key)
-            grid = Grid(path)
-            n_of_animals = f"n_{key}"
-            self.region = grid.get_region_stat(regions=self.region, col_name=n_of_animals, stat="sum")
-            self.region.loc[self.region[n_of_animals] < 0, n_of_animals] = 0  # set negative n of animals to 0
-            kg_per_animal = animal_yield.loc[
-                (animal_yield["animal"] == key) & (animal_yield["Area"] == self.cfg["main"]["country"]), "kg_per_animal"
-            ]
-            logging.info("Calculating the total kg of meat for %s", key)
-            total_kg = kg_per_animal.to_numpy()[0] * self.region[n_of_animals]
-            self.region[f"{key}_kg"] = total_kg
-
     def add_rice_yield(self) -> None:
         """Add rice yield to self.region GeoDataFrame."""
         path = self.cfg["food_production"]["rice_yield"]["path"]
-        logging.info("Parsing rice yield data from his file.")
+        logger.info("Parsing rice yield data from his file.")
         hisfile = HisFile(path).read()
         rice_yield_df = hisfile.to_table(year=self.cfg["main"]["year"])
         if rice_yield_df:
-            logging.info("Adding rice yield to regions.")
+            logger.info("Adding rice yield to regions.")
             self.region = self.region.merge(rice_yield_df, how="left", on="Name")
 
-    def add_other_crops(self) -> None:
-        """Add other crops data to region geodataframe."""
-        paths = self.cfg["food_production"]["other_crops"].get("paths")
-        if not paths:
-            logging.warning("No other crops data found")
-            return
-
-        for key, path in paths.items():
-            data = pd.read_csv(path)
-            data[key] = pd.to_numeric(data[key], errors="coerce")
-            data = data.dropna()
-            data[key] = data[key] * 1e6  # convert thousands of tons to kg
-            join_column = "Name"
-            if col := self.cfg["food_production"]["other_crops"].get("join_column"):
-                join_column = col
-            self.region = self.region.merge(data, left_on="Name", right_on=join_column, how="left")
-            logging.info("Added %s to region GeoDataFrame", key)
-            if join_column != "Name":
-                self.region = self.region.drop(columns=join_column)
-
-    def add_aquaculture(self):
-        pass
+    def add_aquaculture(self) -> None:
+        """Add aquaculture production."""
+        raise NotImplementedError
 
     def run(self) -> gpd.GeoDataFrame:
         """Run the food production methods for adding data to a GeoDataFrame."""
--- food_security/food_supply.py
+++ food_security/food_supply.py
@@ -6,7 +6,6 @@ import geopandas as gpd
 import pandas as pd
 
 from food_security.base import FSBase
-from food_security.fao_api import get_food_export_df, get_food_production_df
 
 logger = logging.getLogger(__name__)
 
@@ -15,68 +14,27 @@ class FoodSupply(FSBase):
     """Calculate the total food supply based on import and export and food production."""
 
     def __init__(self, cfg: dict, region: gpd.GeoDataFrame) -> None:
+        """Instantiate a FoodSupply object."""
         super().__init__(cfg=cfg)
         self.region = region
-        self.export_df = get_food_export_df(country_name=self.cfg["main"]["country"], year=self.cfg["main"]["year"])
-        self.total_production_FAO = get_food_production_df(
-            country_name=self.cfg["main"]["country"], year=self.cfg["main"]["year"]
+        self.fs_data_table = pd.read_csv(
+            self.config["food_security_data_table"]["path"],
         )
 
-    def get_import(self):
-        pass
+    def add_food_supply(self) -> None:
+        """Calculate food supply for regions."""
 
-    def get_export(self):
-        export_df = self.export_df.copy(deep=True)
-        export_df = export_df[
-            (export_df["Year"] == self.cfg["main"]["year"]) & (export_df["Element"] == "Export Quantity")
-        ]
-        food_types = list(self.cfg["food_production"]["lifestock"]["paths"].keys()) + list(
-            self.cfg["food_production"]["other_crops"]["paths"].keys(),
+    def calculate_trade(self) -> gpd.GeoDataFrame:
+        fs_data_table_cfg = self.cfg["food_security_data_table"]
+        trade_gdf = self.region.merge(
+            self.fs_data_table,
+            on=self.cfg["food_security_data_table"]["region"],
+        )
+        trade_gdf["imports"] = (
+            trade_gdf["rice_yield"] * trade_gdf[fs_data_table_cfg["import_coefficient"]]
         )
 
-        # TODO: calculate export of food based on export ratios for country
-
-    def get_road_density(self, geometry: gpd.GeoDataFrame):
-        pass
-
-    def get_GPD_per_capita(self, geometry: gpd.GeoDataFrame):
-        pass
-
-    def calculate_food_supply_for_region(self, geometry: gpd.GeoDataFrame) -> float:
-        pass
-
-    def calculate_food_transfer_coefficient(
-        self,
-        GPD_per_capita,
-        road_density,
-    ) -> float:
-        pass
-
-    def add_food_supply_per_province(
-        self,
-        provinces: gpd.GeoDataFrame,
-        region: gpd.GeoDataFrame,
-    ) -> gpd.GeoDataFrame:
-        food_supply_region = self.calculate_food_supply_for_region(geometry=region)
-
-    def _calculate_export_ratio(self, food_items: list[str]) -> pd.DataFrame:
-        mapping_df = pd.read_csv(self.cfg["food_supply"]["FAO_item_mapping"]["path"])
-        tp = self.total_production_FAO
-        tt = self.export_df
-
-        export_ratios = []
-        for food in food_items:
-            item_code = str(mapping_df[mapping_df["Item"] == food]["Item_code"].to_numpy()[0])
-            total_production = pd.to_numeric(tp[tp["Item Code"] == item_code]["Value"])
-            total_export = pd.to_numeric(tt[tt["Item Code"] == item_code]["Value"]).sum()
-            export_ratio = total_export / total_production
-            export_ratios.append({"item": food, "export_ratio": export_ratio})
-        return pd.DataFrame(export_ratios)
-
-    @property
-    def total_production_FAO(self) -> pd.DataFrame:  # noqa: N802
-        return self._total_production_FAO
-
-    @total_production_FAO.setter
-    def total_production_FAO(self, df: pd.DataFrame):  # noqa: N802
-        self._total_production_FAO = df[df["Area"] == self.cfg["main"]["country"]]
+    def run(self) -> gpd.GeoDataFrame:
+        """Run the food supply methods for adding data to the region GeoDataFrame."""
+        super().run()
+        return self.region
