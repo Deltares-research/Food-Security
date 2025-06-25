8fe62dca10a4ae6bb2086fb99ea3a14b986e9001
Tjalling de Jong <tjalling.dejong@deltares.nl>
2025-06-25 10:49:24 +0200

    added test for food transfer coefficient

--- food_security/food_transfer_coefficient.py
+++ food_security/food_transfer_coefficient.py
@@ -17,8 +17,6 @@ class FoodTransferCoefficient(FSBase):
     def add_ftc(self) -> None:
         """Calculate the food transfer coefficient."""
         self.calculate_road_density()
-        self.calculate_gpd()
-        # Calculate the food transfer coefficient
 
     def get_roads(self) -> gpd.GeoDataFrame:
         """Retrieve road features from OMSnx."""
@@ -38,16 +36,19 @@ class FoodTransferCoefficient(FSBase):
         self.region["road_length"] = 0
         for region_name in self.region["Name"].to_numpy():
             region_roads = roads.sjoin(self.region[self.region["Name"] == region_name])
+            # Clip any road geoms that are sticking outside of the region polygon
+            region_roads = gpd.clip(
+                region_roads, self.region[self.region["Name"] == region_name],
+            )
             region_roads = region_roads.to_crs(utm)
             region_roads["length"] = region_roads.length
             self.region.loc[self.region["Name"] == region_name, "road_length"] = (
                 region_roads["length"].sum()
             )
-        self.region["road_density"] = self.region["road_legth"] / self.region["area"]
-
-    def calculate_gpd(self) -> None:
-        """Calculate the GPD for regions of interest."""
-
+        # Road density is calculated by km of roads diveded by area (km2)
+        self.region["road_density"] = (self.region["road_length"] / 1000) / (
+            self.region["area"] / 1e6
+        )
 
     def run(self) -> gpd.GeoDataFrame:
         """Run the FoodTransferCoefficient workflow."""
